515c6784b4d4aeb51f8749f944415b3787897e15
Danny <dannypen@gmail.com>
2020-03-23 15:58:34 +0000

    Final changes to make site launch correctly

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -6,7 +6,7 @@ import play.api.mvc._
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  def index = Action {
+  def index:Action[AnyContent] = Action {
     Ok(views.html.index("Your new application is ready."))
   }
 
