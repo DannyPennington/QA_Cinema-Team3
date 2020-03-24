6b8833a9621be2e4e1ae0272eef91a401da15c55
Danny <dannypen@gmail.com>
2020-03-24 12:35:01 +0000

    Added ScreensController to handle loading the screens page

---
+++ app/controllers/ScreensController.scala
@@ -0,0 +1,12 @@
+package controllers
+
+import javax.inject.Inject
+import play.api.mvc._
+
+class ScreensController @Inject()(cc: ControllerComponents, val mongoService: MongoService) extends AbstractController(cc){
+
+  def screens:Action[AnyContent] = Action {implicit request:Request[AnyContent] =>
+    Ok(views.html.screens())
+  }
+
+}
