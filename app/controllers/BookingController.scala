5e7e5e0cc931dd55f6de0f08d07cb130c72ce944
Danny <dannypen@gmail.com>
2020-03-24 17:29:36 +0000

    Added parseDetails function to read the http body from the booking form POST request. It is still missing the screen type and the number of customers

--- app/controllers/BookingController.scala
+++ app/controllers/BookingController.scala
@@ -9,4 +9,14 @@ class BookingController@Inject()(cc: ControllerComponents, val mongoService: Mon
   def booking:Action[AnyContent] = Action {implicit request:Request[AnyContent] =>
     Ok(views.html.booking())
   }
+
+  def parseDetails:Action[AnyContent] = Action {implicit request:Request[AnyContent] =>
+    val body = request.body.asFormUrlEncoded
+    val film = body.get("film").head
+    val date = body.get("date").head
+    val time = body.get("time").head
+    val user = body.get("user").head
+    Ok(views.html.details(film,user,time,date))
+
+  }
 }
