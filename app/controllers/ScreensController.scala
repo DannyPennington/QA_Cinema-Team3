10b4a3ce4369932bfea64c238d9c994ec0bb2908
Danny <dannypen@gmail.com>
2020-03-24 14:05:01 +0000

    Added singleton annotation

--- app/controllers/ScreensController.scala
+++ app/controllers/ScreensController.scala
@@ -3,6 +3,7 @@ package controllers
 import javax.inject.Inject
 import play.api.mvc._
 
+@Singleton
 class ScreensController @Inject()(cc: ControllerComponents, val mongoService: MongoService) extends AbstractController(cc){
 
   def screens:Action[AnyContent] = Action {implicit request:Request[AnyContent] =>
