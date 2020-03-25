b49b598dc2c9ee94c44825c4d710005b51524046
AIbrahimQA <57941383+AIbrahimQA@users.noreply.github.com>
2020-03-25 11:28:47 +0000

    Update app/controllers/classifications.scala
    Co-Authored-By: Daniel Pennington <51288183+DannyPennington@users.noreply.github.com>

--- app/controllers/classifications.scala
+++ app/controllers/classifications.scala
@@ -4,7 +4,7 @@ import javax.inject._
 import play.api.mvc._
 
 @Singleton
-class classifications @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class Classifications @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   def createClassifications:Action[AnyContent] = Action {
     Ok(views.html.classifications())
