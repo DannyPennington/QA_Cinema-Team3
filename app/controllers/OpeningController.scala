b0f84c50332689b44a837ff1022b01d1b8a3f126
Danny <dannypen@gmail.com>
2020-03-26 10:13:14 +0000

    Renames OpeningController to match convention and added linking to classification page

--- app/controllers/OpeningController.scala
+++ app/controllers/OpeningController.scala
@@ -2,7 +2,7 @@ package controllers
 import javax.inject._
 import play.api.mvc._
 
-class OpeningTime @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class OpeningController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
   def opening:Action[AnyContent] = Action {implicit request:Request[AnyContent] =>
     Ok(views.html.opening())
   }
