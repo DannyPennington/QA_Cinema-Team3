81308425fee0a397cdc177c4e5863c3c667ca441
Danny <dannypen@gmail.com>
2020-03-26 17:06:03 +0000

    Created RegistrationController which controls all the functions for registration.

---
+++ app/controllers/RegistrationController.scala
@@ -0,0 +1,98 @@
+package controllers
+
+import javax.inject._
+import play.api.mvc._
+import reactivemongo.play.json.collection.JSONCollection
+import reactivemongo.play.json._
+
+import scala.concurrent.{Await, ExecutionContext, Future}
+import models.{Registration, User}
+import models.JsonFormats._
+import play.api.libs.json._
+import reactivemongo.api.Cursor
+import scala.concurrent.duration.Duration
+
+
+class RegistrationController @Inject()(
+                                    components: ControllerComponents,
+                                    val mongoService: MongoService
+                                  ) extends AbstractController(components) with play.api.i18n.I18nSupport {
+
+  implicit def ec: ExecutionContext = components.executionContext
+  val collection: Future[JSONCollection] = mongoService.userCollection
+
+  def searchHelper(email: String): Future[List[User]] = {
+    val cursor: Future[Cursor[User]] = collection.map {
+      _.find(Json.obj("email" -> email)).
+        sort(Json.obj("email" -> -1)).
+        cursor[User]()
+    }
+    val futureUsersList: Future[List[User]] =
+      cursor.flatMap(
+        _.collect[List](
+          -1,
+          Cursor.FailOnError[List[User]]()
+        )
+      )
+    futureUsersList
+  }
+
+  def addUser(username :String, email :String, password :String): Action[AnyContent] = Action.async { implicit request:Request[AnyContent] =>
+    val user = User(username, email, password)
+    val exists = Await.result(userExists(email), Duration.Inf)
+    if (exists) {
+      Future(Redirect(routes.RegistrationController.showRegistration()).flashing("new" -> "no"))
+    }
+    else {
+      val futureResult = collection.flatMap(_.insert.one(user))
+      futureResult.map(_ => Redirect(routes.RegistrationController.success))
+    }
+  }
+
+  def showRegistration: Action[AnyContent] = Action {implicit request:Request[AnyContent] =>
+    if (request.flash.get("new").isDefined) {
+      Ok(views.html.registration(Registration.RegistrationForm, "Email address already registered with account!"))
+    }
+    else if (request.flash.get("exists").isDefined) {
+      Ok(views.html.registration(Registration.RegistrationForm, "Please create an account!"))
+    }
+    else if (request.flash.get("home").isDefined) {
+      Ok(views.html.registration(Registration.RegistrationForm, "Please log in to see your homepage!"))
+    }
+    else{
+      Ok(views.html.registration(Registration.RegistrationForm,""))
+    }
+  }
+
+  def registerUser: Action[AnyContent] = Action { implicit request:Request[AnyContent] =>
+    Registration.RegistrationForm.bindFromRequest.fold({ formWithErrors =>
+      BadRequest(views.html.registration(formWithErrors,""))
+    }, { register =>
+      Redirect(routes.RegistrationController.addUser(register.username, register.email, register.password))
+
+    })
+  }
+
+  def findByEmail(email: String): Action[AnyContent] = Action.async {
+    val futureUsersList = searchHelper(email)
+    futureUsersList.map { persons =>
+      Ok(persons.toString)
+    }
+  }
+
+  def userExists(email: String): Future[Boolean] = {
+    val futureUsersList = searchHelper(email)
+    futureUsersList.map { person =>
+      if (person.isEmpty) {
+        false
+      }
+      else {
+        true
+      }
+    }
+  }
+  def success:Action[AnyContent] = Action {
+    Ok(views.html.message("Thanks for registering!"))
+  }
+
+}
