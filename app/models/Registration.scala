8e3158795e0b4b6c5954b208917ed424a4b63850
Danny <dannypen@gmail.com>
2020-03-26 17:06:52 +0000

    Changed registration form to take a username instead of first and last names

--- app/models/Registration.scala
+++ app/models/Registration.scala
@@ -2,13 +2,12 @@ package models
 import play.api.data.Form
 import play.api.data.Forms._
 
-case class Registration(forename:String, surname:String, email: String, password: String)
+case class Registration(username: String, email: String, password: String)
 
 object Registration {
   val RegistrationForm:Form[Registration] = Form(
     mapping(
-      "forename" -> nonEmptyText,
-      "surname" -> nonEmptyText,
+      "username" -> nonEmptyText,
       "email" -> email,
       "password" -> nonEmptyText
     )(Registration.apply)(Registration.unapply)
