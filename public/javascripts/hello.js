18b900c2b3faa5eb89135fcc22c4d13e0d72eab3
Tola12343 <tolaainabadejo@yahoo.co.uk>
2020-03-27 11:05:44 +0000

    Changes made to Jquery

--- public/javascripts/hello.js
+++ public/javascripts/hello.js
@@ -1,15 +1,15 @@
 if (window.console) {
   console.log("Welcome to your Play application's JavaScript!");
 }
-$('#modal1').on('hidden.bs.modal', function (e) {
-  $('#modal1 iframe').attr("src", $("#modal1 iframe").attr("src"));
+$("#modal").on("hidden.bs.modal", function (e) {
+  $("#modal1 iframe").attr("src", $("#modal1 iframe").attr("src"));
 });
-$('#modal2').on('hidden.bs.modal', function (e) {
-  $('#modal2 iframe').attr("src", $("#modal2 iframe").attr("src"));
+$("#modal2").on("hidden.bs.modal", function (e) {
+  $("#modal2 iframe").attr("src", $("#modal2 iframe").attr("src"));
 });
-$('#modal3').on('hidden.bs.modal', function (e) {
-  $('#modal3 iframe').attr("src", $("#modal3 iframe").attr("src"));
+$("#modal3").on("hidden.bs.modal", function (e) {
+  $("#modal3 iframe").attr("src", $("#modal3 iframe").attr("src"));
 });
-$('#modal4').on('hidden.bs.modal', function (e) {
-  $('#modal4 iframe').attr("src", $("#modal4 iframe").attr("src"));
+$("#modal4").on("hidden.bs.modal", function (e) {
+  $("#modal4 iframe").attr("src", $("#modal4 iframe").attr("src"));
 });
