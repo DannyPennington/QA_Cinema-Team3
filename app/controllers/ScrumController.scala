b684d9520b2d1dcc2e6e9e96d28f09249cf048f2
Johannes <Johannes.Bull@academytrainee.com>
2020-03-31 12:27:28 +0100

    Fixed one minor issue

--- app/controllers/ScrumController.scala
+++ app/controllers/ScrumController.scala
@@ -10,7 +10,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 @Singleton
 class ScrumController @Inject()(cc: ControllerComponents, val mongoService: MongoService) extends AbstractController(cc) with I18nSupport {
 
-  def scrum: Action[AnyContent] = Action {
+  def scrum: Action[AnyContent] = Action {implicit request:Request[AnyContent] =>
     Ok(views.html.scrum())
   }
 
