f982e449c3d714f62e9f019934806b7a6569e443
Sam White <sw16789@my.bristol.ac.uk>
2020-04-01 12:52:32 +0100

    profanity filter updated, default rating added

--- app/controllers/DiscussionController.scala
+++ app/controllers/DiscussionController.scala
@@ -9,6 +9,7 @@ import scala.collection.mutable.ListBuffer
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.io.Source
 
 
 class DiscussionController @Inject()(cc: ControllerComponents, val mongoService: MongoService) extends AbstractController(cc) with I18nSupport {
@@ -18,11 +19,16 @@ class DiscussionController @Inject()(cc: ControllerComponents, val mongoService:
   }
 
   def parseDetails: Action[AnyContent] = Action.async { implicit request: Request[AnyContent] =>
-    val badWords = List("shit","fuck")
+
+    val bufferedSource = Source.fromFile("public/profanityList.txt")
+    val badWords = new ListBuffer[String]()
+    for (line <- bufferedSource.getLines) badWords += line
+    bufferedSource.close()
+
     val body = request.body.asFormUrlEncoded
     val review = body.get("review").head.split(" ")
     var safeReview = new ListBuffer[String]()
-    review.foreach(word => if (badWords.contains(word)) safeReview += (word.replaceAll("[a-zA-Z]","*")) else safeReview += word)
+    review.foreach(word => if (badWords.contains(word.toLowerCase)) safeReview += (word.replaceAll("[a-zA-Z]","*")) else safeReview += word)
 
     mongoService.createDiscussion(DiscussionEntry(body.get("film").head, body.get("rating").head.toInt, safeReview.toList.mkString(" "))).map(
       _ => Redirect(routes.DiscussionController.discussion())
