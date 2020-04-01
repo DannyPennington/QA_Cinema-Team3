fff07d71546a22c9e074b6bbe36396e9173c818e
Danny <dannypen@gmail.com>
2020-04-01 12:11:26 +0100

    Changed test text

--- test/controllers/BookingControllerTest.scala
+++ test/controllers/BookingControllerTest.scala
@@ -16,8 +16,8 @@ class BookingControllerTest extends PlaySpec with Results with MockitoSugar {
 
   implicit val ec: ExecutionContextExecutor = ExecutionContext.global
 
-  "Page#booking with template" should {
-    "should be valid" in {
+  "Booking page" should {
+    "get data from the database to display in the html" in {
       val currentMovieList = Future[List[MovieInfo]](List(MovieInfo("Mulan","Tony Bancroft",List("Ming-Na Wen","Eddie Murphy","BD Wong"),List("10:00","11:30","14:00","17:30"),"https://m.media-amazon.com/images/M/MV5BODkxNGQ1NWYtNzg0Ny00Yjg3LThmZTItMjE2YjhmZTQ0ODY5XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg")))
       val mongoService = mock[MongoService]
       when(mongoService.findCurrentMovies()).thenReturn(currentMovieList)
