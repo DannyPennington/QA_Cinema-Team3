b10afaf7f6be52b3c4bb6bd004567e50d7513938
Danny <dannypen@gmail.com>
2020-04-02 15:19:30 +0100

    Removed unused val

--- test/controllers/BookingControllerTest.scala
+++ test/controllers/BookingControllerTest.scala
@@ -20,7 +20,6 @@ class BookingControllerTest extends PlaySpec with Results with MockitoSugar {
   "Booking page" should {
     "get data from the database to display in the html" in {
       val currentMovieList = Future[List[MovieInfo]](List(MovieInfo("Mulan","Tony Bancroft",List("Ming-Na Wen","Eddie Murphy","BD Wong"),List("10:00","11:30","14:00","17:30"),"https://m.media-amazon.com/images/M/MV5BODkxNGQ1NWYtNzg0Ny00Yjg3LThmZTItMjE2YjhmZTQ0ODY5XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg")))
-      val mongoService = mock[MongoService]
       when(mongoService.findCurrentMovies()).thenReturn(currentMovieList)
       val controller = new BookingController(Helpers.stubControllerComponents(), mongoService)
       val result: Future[Result] = controller.booking().apply(FakeRequest())
